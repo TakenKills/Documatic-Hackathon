6dfdd3348a271c6c8d9252fd02bcda7a7b628864
TakenKills <dragonagario2@gmail.com>
2022-01-08 21:09:10 +0200

    Change cooldowns

--- src/Struct/Event/events/cooldown.ts
+++ src/Struct/Event/events/cooldown.ts
@@ -2,15 +2,34 @@ import { EventBase } from "../EventBase";
 import { Message } from "eris";
 import { CommandBase as Command } from "../../Command/CommandBase";
 
+function HumanTime(sec: number) {
+	const hours = Math.floor(sec / 3600);
+	const minutes = Math.floor((sec - hours * 3600) / 60);
+	const seconds = sec - hours * 3600 - minutes * 60;
+
+	let time = "";
+	if (hours > 0) {
+		time += `${hours} hour${hours > 1 ? "s" : ""}`;
+	}
+	if (minutes > 0) {
+		if (time !== "") time += ", ";
+		time += `${minutes} minute${minutes > 1 ? "s" : ""}`;
+	}
+	if (seconds > 0) {
+		if (time !== "") time += ", ";
+		time += `${seconds} second${seconds > 1 ? "s" : ""}`;
+	}
+
+	return time;
+}
+
 export = class Cooldown extends EventBase {
 	public constructor() {
 		super({ name: "cooldown", emitter: "commandhandler" });
 	}
 
 	public execute(message: Message, _: Command, timeLeft: number): void {
-		const minutes = timeLeft > 60 ? Math.floor(timeLeft / 60) : 0;
-		const seconds = Math.floor(timeLeft - minutes * 60);
-		const time = `${minutes > 10 ? minutes : `0${minutes}`}:${seconds > 10 ? seconds : `0${seconds}`}m`;
+		const time = HumanTime(timeLeft);
 
 		this.client.createMessage(message.channel.id, {
 			content: `you have to wait \`${time}\` before using this command again.`,
